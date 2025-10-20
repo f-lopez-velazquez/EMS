14442b056300d4964b9b8546ec83ae1b3bb38f0b
f-lopez-velazquez <f.lopezvelazquez@ugto.mx>
2025-10-19 23:01:40 -0600

    fix: cotización – limpiar enviarCotizacion(), exportar funciones globales y bust cache
    - Reescribe enviarCotizacion sin bloque ajeno incrustado
    - Exporta guardarCotizacionDraft/generarPDFCotizacion/previsualizar* a window
    - SW v43 y app.js?v=43 para forzar actualización

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-﻿const CACHE_NAME = 'ems-cache-v42';
+﻿const CACHE_NAME = 'ems-cache-v43';
 const toCache = [
   './',
   './index.html',
@@ -111,4 +111,5 @@ self.addEventListener('message', event => {
     self.skipWaiting();
   }
 });
+
 
